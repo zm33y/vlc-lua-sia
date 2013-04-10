70738ae564b6e92ce5334ac87ba23a839f84a7d5
dmitry.shesterkin <dfb@yandex.ru>
2013-04-10 21:29:38 +0400

    Reverted original settings

--- say_it_again.lua
+++ say_it_again.lua
@@ -54,13 +54,12 @@ Abbreviations used in code:
 --[[  Settings  ]]--
 local sia_settings =
 {
-    --charset = "iso-8859-1", -- works for english and french subtitles (try also "Windows-1252")
-    charset = nil,          -- UT8 encoding
-    dict_dir = "c:/!MY_DOCS/say_it_again/dict",            -- where Stardict dictionaries are located
-    wordnet_dir = "c:/!MY_DOCS/say_it_again/WordNet", -- where WordNet files are located
-    --chosen_dict = "c:/!MY_DOCS/say_it_again/dict/Oxford Advanced Learner's Dictionary", -- Stardict dictionary used by default (there should be 3 files with this name but different extensions)
-    chosen_dict = "c:/!MY_DOCS/say_it_again/dict/OxfordAmericanDictionaryEnEn", -- Stardict dictionary used by default (there should be 3 files with this name but different extensions)
-    words_file_path = "c:/!MY_DOCS/say_it_again/sia_words.txt", -- if 'nil' then "Desktop/sia_words.txt" will be used
+    charset = "iso-8859-1",          -- works for english and french subtitles (try also "Windows-1252")
+    --charset = nil,                 -- UTF8 encoding
+    dict_dir = "C:/dict",            -- where Stardict dictionaries are located
+    wordnet_dir = "C:/dict/wordnet", -- where WordNet files are located
+    chosen_dict = "C:/dict/OxfordAmericanDictionaryEnEn", -- Stardict dictionary used by default (there should be 3 files with this name but different extensions)
+    words_file_path = nil, -- if 'nil' then "Desktop/sia_words.txt" will be used
     always_show_subtitles = false,
     osd_position = "top",
     help_duration = 6, -- sec; change to nil to disable osd help
@@ -74,7 +73,7 @@ local sia_settings =
     key_always_show_subs = 114, -- R
     
     -- global subtitles time shift to compensate vlc audio start delay or lack of subtitles synchronization
-    subs_time_shift = -2.5
+    subs_time_shift = 0
 }
 
 
